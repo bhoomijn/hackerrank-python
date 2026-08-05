7da7bdded495e239b5b273ba486e5048e8d3b80e
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-05 10:34:45 +0530

    Update and rename py to hello_world.py

---
+++ Introduction/hello_world.py
@@ -0,0 +1,11 @@
+
+# HackerRank Challenge: Say "Hello, World!" With Python
+# Author: Bhoomi
+# Language: Python 3
+
+def main():
+    # Print the classic Hello, World! message
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
