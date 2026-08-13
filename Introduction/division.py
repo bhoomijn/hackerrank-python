b839237d678ae872be121f97639b566d58e295f0
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-13 15:26:48 +0530

    Create division.py

---
+++ Introduction/division.py
@@ -0,0 +1,9 @@
+# Division Problem - HackerRank
+
+# Read two integers from STDIN
+a = int(input())
+b = int(input())
+
+# Print results
+print(a // b)   # Integer division
+print(a / b)    # Float division
