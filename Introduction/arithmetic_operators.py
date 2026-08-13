f349a8038321b8f4bc08cfac9986168de79596d3
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-13 15:13:41 +0530

    Create arithmetic_operators.py

---
+++ Introduction/arithmetic_operators.py
@@ -0,0 +1,10 @@
+# Arithmetic Operators Problem - HackerRank
+
+# Read two integers from STDIN
+a = int(input())
+b = int(input())
+
+# Print results
+print(a + b)   # Sum
+print(a - b)   # Difference
+print(a * b)   # Product
