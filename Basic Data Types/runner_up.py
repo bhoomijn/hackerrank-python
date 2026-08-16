dc062471fd98544544b36a1ac9a8a137b0376f43
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-16 19:15:29 +0530

    Update runner_up.py

--- Basic Data Types/runner_up.py
+++ Basic Data Types/runner_up.py
@@ -1,1 +1,14 @@
 
+# runner_up.py
+
+n = int(input())                      # number of participants
+scores = list(map(int, input().split()))  # scores list
+
+# duplicates remove karo
+unique_scores = list(set(scores))
+
+# ascending sort
+unique_scores.sort()
+
+# second last element hi runner-up hoga
+print(unique_scores[-2])
